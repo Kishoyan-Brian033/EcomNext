17009dc159d8b5eb20b6797ec32ffad68747439a
Kishoyan-Brian033 <brianmwasbayo@gmail.com>
2025-04-24 18:01:31 +0300

    update index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
+'use strict';
 // modal variables
 const modal = document.querySelector('[data-model]');
-const modalCloseBtn = document.querySelector('[data-model-close]');
-const modalOverlay = document.querySelector('[data-model-overlay]');
-
-// modal open function
-const modalCloseFunc = function () {
+const modelCloseBtn = document.querySelector('[data-model-close-btn]');
+const modelCloseOverlay = document.querySelector('[data-model-overlay]');
+// model functions
+const modelCloseFunc = function () {
     modal.classList.add('closed');
 }
-// add event listener 
-modalOverlay.addEventListener('click',modalCloseFunc);
-modalCloseBtn.addEventListener('click',modalCloseFunc);
+// add eventlistener
+modelCloseOverlay.addEventListener('click', modelCloseFunc);
+modelCloseBtn.addEventListener('click', modelCloseFunc);
+
+
+
+
+// notification variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[ data-toast-close]');
+
+// adding event listener
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+})
